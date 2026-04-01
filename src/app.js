a2d984251a38ed1377bceff03dc33a6e4c0397b5
anmobd111 <sanmobd111@gmail.com>
2026-04-02 01:09:17 +0600

    setup arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import logger from '#config/logger.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.route.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -20,6 +21,8 @@ app.use(
     stream: { write: message => logger.info(message.trim()) },
   })
 );
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Received request for root endpoint');
